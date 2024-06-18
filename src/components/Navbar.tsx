f3b8c043fa18276377b0ad8fab2579a8ba223977
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-06-18 21:16:05 +1200

    updated the navigation bar with icons and hover effects

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,8 +9,14 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
+
 import SoupKitchenIcon from "@mui/icons-material/SoupKitchen";
 import MenuIcon from "@mui/icons-material/Menu";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 import "../styles/Navbar.css";
 
@@ -30,95 +36,108 @@ const Navbar: React.FC = () => {
         sx={{ flexGrow: 1, my: 2 }}
       >
         <SoupKitchenIcon />
-        BunShack
+        BunShack Breakfast
       </Typography>
       <Divider />
       <ul className="mobile-navigation">
         <li>
           <Link to="/">
+            <HomeIcon />
             Home
           </Link>
         </li>
         <li>
-          <Link to="/login">About</Link>
+          <Link to="/about">
+            <PeopleAltIcon />
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/register">Contact</Link>
+          <Link to="/menu">
+            <MenuBookIcon />
+            Menu
+          </Link>
         </li>
         <li>
-          <Link to="/order">Order</Link>
+          <Link to="/contact">
+            <InfoIcon />
+            Contact
+          </Link>
         </li>
         <li>
-          <Link to="/profile">Profile</Link>
+          <Link to="/order">
+            <ShoppingCartIcon />
+            Order
+          </Link>
         </li>
       </ul>
     </Box>
   );
   return (
-    <Box>
-      <AppBar component={"nav"} sx={{ bgcolor: "black" }}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            sx={{ mr: 2, display: { sm: "none" } }}
-            onClick={handleDrawerToggle}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            color={"goldenrod"}
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1 }}
-          >
-            <SoupKitchenIcon />
-            BunShack
-          </Typography>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            <ul className="navigation-menu">
-              <li>
-                <Link to="/">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to="/login">About</Link>
-              </li>
-              <li>
-                <Link to="/register">Contact</Link>
-              </li>
-              <li>
-                <Link to="/order">Order</Link>
-              </li>
-              <li>
-                <Link to="/profile">Profile</Link>
-              </li>
-            </ul>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <Box component="nav">
-        <Drawer
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: "240px",
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </Box>
+    <>
       <Box>
-        <Toolbar />
+        <AppBar component={"nav"} sx={{ bgcolor: "#1A1A19" }}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              sx={{ mr: 2, display: { sm: "none" } }}
+              onClick={handleDrawerToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              color={"goldenrod"}
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+              <SoupKitchenIcon />
+              BunShack Breakfast
+            </Typography>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              <ul className="navigation-menu">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/menu">Menu</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+                <li>
+                  <Link to="/order">Order</Link>
+                </li>
+              </ul>
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Box component="nav">
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: "240px",
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box>
+          <Toolbar />
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
