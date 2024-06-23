0ec4a0ed22268c38b300748e25285a78567832bf
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-06-23 21:32:19 +1200

    Create a MenuList component

---
+++ src/components/MenuList.tsx
@@ -0,0 +1,99 @@
+import Congee from "../assets/Congee.jpg";
+import FriedDumping from "../assets/FriedDumpling.jpg";
+import PorkBuns from "../assets/PorkBuns.jpg";
+import JelliedBeancurd from "../assets/JelliedBeancurd.jpg";
+import ShrimpDumpling from "../assets/ShrimpDumplings.jpg";
+import SoyMilk from "../assets/SoyMilk.jpg";
+import SpicyBeefNoodle from "../assets/SpicyBeefNoodle.jpg";
+import WontonSoup from "../assets/WontonSoup.jpg";
+import SpicyMeatPasteNoodle from "../assets/SpicyMeatPasteNoodle.jpg";
+import WontonChilliOil from "../assets/WontonChilliOil.jpg";
+import FriedBuns from "../assets/FriedBuns.jpg";
+import WontonNoodle from "../assets/WontonNoodle.jpg";
+
+export const MenuList = [
+  {
+    name: "Rice Congee",
+    description:
+      "Congee is a type of rice porridge. Additional meat balls are added on top.",
+    image: Congee,
+    price: 5.99,
+  },
+  {
+    name: "Fried Dumplings",
+    description:
+      "Fried dumplings are a type of dumpling that are pan-fried instead of boiled or steamed.",
+    image: FriedDumping,
+    price: 11.99,
+  },
+  {
+    name: "Pork Buns",
+    description:
+      "Popular in Chinese cuisine, pork buns are a type of bun that are filled with pork and served as steamed.",
+    image: PorkBuns,
+    price: 2.99,
+  },
+  {
+    name: "Fried Buns",
+    description:
+      "Fried buns are a type of bun that are filled with meat and vegetables and served as fried.",
+    image: FriedBuns,
+    price: 7.99,
+  },
+  {
+    name: "Jellied Beancurd",
+    description:
+      "Jellied beancurd is a type of tofu that is served with savoury or sweet toppings.",
+    image: JelliedBeancurd,
+    price: 6.99,
+  },
+  {
+    name: "Shrimp Dumpling",
+    description:
+      "Shrimp dumplings are a type of dumpling that are filled with shrimp and served as steamed.",
+    image: ShrimpDumpling,
+    price: 9.99,
+  },
+  {
+    name: "Soy Milk",
+    description:
+      "Soy milk is made from fresh soybeans and healthy as it is unsweetened.",
+    image: SoyMilk,
+    price: 3.99,
+  },
+  {
+    name: "Spicy Beef Noodle Soup",
+    description:
+      "Spicy beef noodle soup is handmade noodles served with beef, spicy broth, and vegetables.",
+    image: SpicyBeefNoodle,
+    price: 12.99,
+  },
+  {
+    name: "Wonton Soup",
+    description:
+      "Wonton soup is a type of dumpling that is filled with chicken and shrimp and served in a clear broth.",
+    image: WontonSoup,
+    price: 10.99,
+  },
+  {
+    name: "Wonton Noodle Soup",
+    description:
+      "Wonton served with egg noddles and vegetables in a clear chicken broth.",
+    image: WontonNoodle,
+    price: 13.99,
+  },
+  {
+    name: "Wonton in Chilli Oil",
+    description:
+      "Wonton in chilli oil is a spicy dish where wonton is served with a spicy chilli oil sauce.",
+    image: WontonChilliOil,
+    price: 8.99,
+  },
+  {
+    name: "Spicy Meat Paste Noodle Set",
+    description:
+      "Homemade noodles served with spicy minced meat paste, vegetables, and special side dishes.",
+    image: SpicyMeatPasteNoodle,
+    price: 14.99,
+  },
+];
